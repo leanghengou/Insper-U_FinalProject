8dad215f18d8d083c571498e253c13f8cac0a81f
leanghengou <leanghengou5555@gmail.com>
2022-08-19 21:50:57 -0400

    batchImport

---
+++ server/batchImport.js
@@ -0,0 +1,25 @@
+const { articles } = require("./data");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const batchImport = async (dbName) => {
+  try {
+    const client = new MongoClient(MONGO_URI, options);
+    const db = client.db(dbName);
+    await client.connect();
+    await db.collection("articles").insertOne(articles);
+    // await db.collection("flightseats").insertMany(flightData);
+    // await db.collection("reservations").insertMany(correctedReservations);
+    // await db.collection("reservations").deleteMany({ flight: "SA231" });
+    client.close();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+batchImport("insperu");
