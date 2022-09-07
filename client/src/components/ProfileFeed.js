614e809667a13df0862c6a2ae84f47462e721a3b
leanghengou <leanghengou5555@gmail.com>
2022-09-06 22:00:41 -0400

    Profile feed (Profile, put some text for empty recent comments and like)

--- client/src/components/ProfileFeed.js
+++ client/src/components/ProfileFeed.js
@@ -30,61 +30,104 @@ const ProfileFeed = ({ recentLike, recentComment }) => {
   return (
     <Container>
       <BoxContainer>
-        <Subtitle>Liked articles</Subtitle>
-        {likeFeed &&
-          likeFeed.reverse().map((article) => {
-            let category = article && article.category[0];
-            if (category === "personal-development") {
-              category = "Personal development";
-            }
-            if (category === "life-tip") {
-              category = "Life tips";
-            }
-            if (category === "Personal story") {
-              category = "Personal story";
-            } else {
-              category = category.charAt(0).toUpperCase() + category.slice(1);
-            }
-
-            return (
-              <LinkContainer>
-                <ArticleName
-                  onClick={(e) => {
-                    navigate(`/article/${article._id}`);
-                  }}
-                >
-                  {article.title}
-                </ArticleName>
-                <ArticleCategory>{category}</ArticleCategory>
-              </LinkContainer>
-            );
-          })}
+        {likeFeed && likeFeed.length <= 0 ? (
+          <EmptyLike />
+        ) : (
+          <LikeBox likeFeed={likeFeed && likeFeed} navigate={navigate} />
+        )}
       </BoxContainer>
-      <BoxContainer>
-        <Subtitle>Recent comments</Subtitle>
-        {commentFeed &&
-          commentFeed.reverse().map((comment) => {
-            const commentText = comment.comment.slice(0, 120);
-            return (
-              <LinkContainer>
-                <ArticleName
-                  onClick={(e) => {
-                    navigate(`/article/${comment.articleId}`);
-                  }}
-                >
-                  {comment.comment.length > 120
-                    ? `"${commentText}..."`
-                    : `"${comment.comment}"`}
-                </ArticleName>
-                <ArticleCategory>{comment.articleTitle}</ArticleCategory>
-              </LinkContainer>
-            );
-          })}
-      </BoxContainer>
+      {commentFeed && commentFeed.length <= 0 ? (
+        <EmptyComment />
+      ) : (
+        <CommentsBox
+          commentFeed={commentFeed && commentFeed}
+          navigate={navigate}
+        />
+      )}
     </Container>
   );
 };
+// -------------------------------------------------------
 
+const EmptyLike = () => {
+  return (
+    <BoxContainer>
+      <Subtitle>Liked articles</Subtitle>
+      <EmptyText>User haven't liked on any article yet.</EmptyText>
+    </BoxContainer>
+  );
+};
+
+const LikeBox = ({ likeFeed, navigate }) => {
+  return (
+    <BoxContainer>
+      <Subtitle>Liked articles</Subtitle>
+      {likeFeed &&
+        likeFeed.reverse().map((article) => {
+          let category = article && article.category[0];
+          if (category === "personal-development") {
+            category = "Personal development";
+          }
+          if (category === "life-tip") {
+            category = "Life tips";
+          }
+          if (category === "Personal story") {
+            category = "Personal story";
+          } else {
+            category = category.charAt(0).toUpperCase() + category.slice(1);
+          }
+
+          return (
+            <LinkContainer>
+              <ArticleName
+                onClick={(e) => {
+                  navigate(`/article/${article._id}`);
+                }}
+              >
+                {article.title}
+              </ArticleName>
+              <ArticleCategory>{category}</ArticleCategory>
+            </LinkContainer>
+          );
+        })}
+    </BoxContainer>
+  );
+};
+// ------------------------------------------------------
+const EmptyComment = () => {
+  return (
+    <BoxContainer>
+      <Subtitle>Recent comments</Subtitle>
+      <EmptyText>User haven't commented on any article yet.</EmptyText>
+    </BoxContainer>
+  );
+};
+const CommentsBox = ({ commentFeed, navigate }) => {
+  return (
+    <BoxContainer>
+      <Subtitle>Recent comments</Subtitle>
+      {commentFeed &&
+        commentFeed.reverse().map((comment) => {
+          const commentText = comment.comment.slice(0, 120);
+          return (
+            <LinkContainer>
+              <ArticleName
+                onClick={(e) => {
+                  navigate(`/article/${comment.articleId}`);
+                }}
+              >
+                {comment.comment.length > 120
+                  ? `"${commentText}..."`
+                  : `"${comment.comment}"`}
+              </ArticleName>
+              <ArticleCategory>{comment.articleTitle}</ArticleCategory>
+            </LinkContainer>
+          );
+        })}
+    </BoxContainer>
+  );
+};
+// -------------------------------------------------------
 const Container = styled.div`
   margin-top: 20px;
   width: 100%;
@@ -132,5 +175,11 @@ const ArticleCategory = styled.p`
 const LinkContainer = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const EmptyText = styled.p`
+  font-size: 16px;
+  line-height: 25px;
+  color: #6c6c6c;
 `;
 export default ProfileFeed;
